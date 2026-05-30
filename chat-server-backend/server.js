78fae229712e90a1890c3b5c92d2a0a8cdffd155
Saood <syedsaoodmian835@gmial.com>
2026-05-30 12:42:01 +0530

    Update socket server configuration

--- chat-server-backend/server.js
+++ chat-server-backend/server.js
@@ -3,54 +3,37 @@ const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
 
-
 const app = express();
+
 app.use(cors());
 
 const server = http.createServer(app);
 
-
-
-
-// we are creating a  socket server
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // React app
+    origin: "http://localhost:5173",
     methods: ["GET", "POST"],
   },
 });
 
-
-
-
-// when user connects
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
-  // receive message from client
   socket.on("send_message", (data) => {
-    console.log("Message:", data)
-    // send message to ALL users 
-    io.emit("receive_message", {
-      username: data.username,
-      message: data.message,
-    });
+    console.log("Received:", data);
+
+    io.emit("receive_message", data);
   });
 
   socket.on("disconnect", () => {
-    console.log("User disconnected");
+    console.log("User disconnected:", socket.id);
   });
 });
 
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
 
-app.get("/", (req, res)=>{
-  console.log("root route hit")
-  res.send("server is running")
-})
-
-
-
-// start the server 3000
 server.listen(3001, () => {
   console.log("Server running on port 3001");
 });
