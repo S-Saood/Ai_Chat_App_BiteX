3227a22fb60f9ca3e1e3586ad8948a12fff73aa6
Saood <syedsaoodmian835@gmial.com>
2026-05-30 12:52:52 +0530

    Add dynamic port for deployment

--- chat-server-backend/server.js
+++ chat-server-backend/server.js
@@ -34,6 +34,8 @@ app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
-server.listen(3001, () => {
-  console.log("Server running on port 3001");
+const PORT = process.env.PORT || 3001;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
